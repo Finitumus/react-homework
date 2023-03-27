18c649b02b91512ee6f991035ab8276c792c4d4a
Finitumus <finitumus@gmail.com>
2023-03-27 10:52:03 +0500

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,71 @@
-import { Routes, Route, Navigate} from 'react-router-dom';
-import { lazy } from 'react';
-import Home from '../page/Home';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Layout from './Layout/Layout';
-import NotFound from '../page/NotFound/NotFound'
+
+import React, { useState, useEffect } from 'react'
+import ContactForm from './Contacts/ContactsForm';
+import ContactList from './Contacts/ContactList';
+import  Filter  from './Contacts/FilterContacts';
 
 
-const Movies = lazy(() => import('../page/Movies'));
-const MoviesDetails = lazy(() => import('../page/MoviesDetails'));
-const Reviews = lazy(() => import('./Reviews'));
-const Cast = lazy(() => import('./Cast'));
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Movies/> } />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:id" element={<MoviesDetails />}>
-            <Route path="cast" element={<Cast />} />
+    const changeFilter = e => {
+      setFilter(e.currentTarget.value );
+    };
 
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-      ;
-      <ToastContainer autoClose={300} />
-    </>
-  );
+    const handleSubmit = obj => {
+     
+      const checkContact = contacts.find(
+        contact => contact.name.toLowerCase() === obj.name.toLowerCase()
+      );
+      if (!checkContact) {
+        setContacts( contacts => [...contacts, obj] );
+        // console.log(obj)
+        return;
+      }
+      alert(`${obj.name} is already in contacts `);
+    }
+
+    const deleteContact = id => {
+      setContacts(contacts => contacts.filter(el => el.id !== id));
+    }
+
+    const visibileContacts = () => {
+     
+      const normalize = filter.toLowerCase();
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalize)
+      );
+    };
+    
+
+      return (
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmit={handleSubmit} contacts={contacts} />
+          <h2>Contacts</h2>
+          <Filter filterName={changeFilter} value={filter} />
+          <ContactList
+            contacts={visibileContacts()}
+            onDelete={deleteContact}
+          />
+        </div>
+      );
+    
 };
+export default App;
 
 
